58e10eb1f8991b458b1c1e88728a709af54b1c99
Andreacolaci99 <colaciandrea99@gmail.com>
2024-12-31 15:46:11 +0100

    feat : edit price

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,17 @@
 console.log(document)
 
-const submitButton = document.querySelector(`#button`)
-const kmToGo = document.querySelector(`#kmToGo`)
 const age = document.querySelector(`#age`)
+const kmToGo = document.querySelector(`#kmToGo`)
+const submitButton = document.querySelector(`#button`)
 const price = document.querySelector(`#price`)
-const km_price = 0.21 ;
-const finalprice = kmToGo * km_price;
-const scontogiovani = finalprice - (finalprice * 0.20);
-const scontoanziani = finalprice - (finalprice * 0.40);
+const km_price = kmToGo * 0.21 ;
+const scontogiovani = km_price - (km_price * 0.20);
+const scontoanziani = km_price - (km_price * 0.40);
 let result = 0;
 
 submitButton.addEventListener(`click`, function(event){
   event.preventDefault()
-  
+
   if (!isNaN(kmToGo.value)){
 
       if (Number(age.value)<18){
@@ -24,7 +23,7 @@ submitButton.addEventListener(`click`, function(event){
      result = scontoanziani ;
  
      }else {
-      result = finalprice ; 
+      result = km_price ; 
     }
      price.innerHTML =`Il prezzo da pagare è ${result}`
    }
