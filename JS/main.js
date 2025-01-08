3c42878d03809d3f902f7c27bc9cea213a07f2a4
Andreacolaci99 <colaciandrea99@gmail.com>
2025-01-08 19:32:14 +0100

    feat : End Exercise

--- JS/main.js
+++ JS/main.js
@@ -1,34 +1,28 @@
 
-const age = document.getElementById(`age`)
 const kmToGo = document.getElementById(`kmToGo`)
 const submitButton = document.getElementById(`button`)
 const price = document.getElementById(`price`)
-const km_price = kmToGo.value * 0.21 ;
-const scontogiovani = km_price - (km_price * 0.20);
-const scontoanziani = km_price - (km_price * 0.40);
 
 
 submitButton.addEventListener(`click`, function(event){
   event.preventDefault()
-  let result = 0;
-  
-  if (!isNaN(kmToGo.value)){
 
-      if (Number(age.value)<18){
+  let km_price = kmToGo.value * 0.21 ;
+  const age = document.querySelector(`#age`)
+  const ageValue = parseInt(age.value)
+  console.log(ageValue)
 
-     let result = scontogiovani;
- 
-     }else if (!isNaN(kmToGo.value) && Number(age.value)>65){
+    if (ageValue < 18){
+
+     km_price *= 0.80;
       
-    let result = scontoanziani;
- 
-     }else {
+    } else if(ageValue > 65){
 
-    let result = km_price;
+    km_price *= 0.60;
 
     }
-     price.innerHTML =`Il prezzo da pagare è ${result.value}`
+    
+     console.log(km_price)
+      price.innerHTML =`Il prezzo da pagare è ${km_price.toFixed(2)}`
    }
-}
 )
-console.log(result)
